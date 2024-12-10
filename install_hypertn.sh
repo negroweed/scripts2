2b1174a82dd37424af2d2038249eed2885baae7f
negroweed <134845563+negroweed@users.noreply.github.com>
2024-12-10 21:55:23 +0800

    last

--- install_hypertn.sh
+++ install_hypertn.sh
@@ -1,12 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-set -ex
+set -e
+
+if ! command -v zstd &>/dev/null; then
+    echo "zstd is not installed, installing zstd...."
+    sleep 1
+    pkg update -y && pkg install zstd -y
+    echo
+    echo "done"
+    sleep 1
+fi
+
+slot="AB"
+
 clear
 
 echo "HyperTN/MIUITN Rom Mod by Thang Nguyen (Based on China ROM)"
-
-fastboot_cmd="META-INF/fastboot"
-
 echo "***********************************************"
 echo ""
 echo "  1. Make sure your computer's hard drive is more than 15 Gb"
@@ -25,8 +34,6 @@ echo ""
 echo ""
 echo "***********************************************"
 
-fqlx="AB"
-
 echo ""
 echo ""
 echo "  Please ignore the prompt 'Invalid sparse file format at magic header'"
@@ -38,7 +45,7 @@ for file in *.new.dat.zst; do
     par=${file%.new.dat.zst}
     rm -f "$par.img"
     echo "  Extracting $par ..."
-    META-INF/zstd -d "$file" -o "$par.img"
+    zstd -d "$file" -o "$par.img"
 done
 
 for file in firmware-update/*; do
@@ -46,28 +53,32 @@ for file in firmware-update/*; do
     par=${par%.*}
 
     if [[ "$par" == "cust" ]]; then
-        $fastboot_cmd flash "$par" "$file"
+        fastboot flash "$par" "$file"
+        
     elif [[ "$par" == "preloader_raw" ]]; then
         for preloader in preloader_a preloader_b preloader1 preloader2; do
-            $fastboot_cmd flash "$preloader" "$file"
+            fastboot flash "$preloader" "$file"
         done
-    elif [[ "$fqlx" == "AB" ]]; then
-        $fastboot_cmd flash "${par}_a" "$file"
-        $fastboot_cmd flash "${par}_b" "$file"
+        
+    elif [[ "$slot" == "AB" ]]; then
+        fastboot flash "${par}_a" "$file"
+        fastboot flash "${par}_b" "$file"
+        
     else
-        $fastboot_cmd flash "$par" "$file"
+        fastboot flash "$par" "$file"
+        
     fi
 done
 
 if [[ -f super.img ]]; then
-    $fastboot_cmd flash super super.img
+    fastboot flash super super.img
     rm -f super.img
 fi
 
 if [[ "$CHOICE" == "n" ]]; then
     echo "  Formatting..."
-    $fastboot_cmd erase userdata
-    $fastboot_cmd erase metadata
+    fastboot erase userdata
+    fastboot erase metadata
     echo ""
 fi
 
@@ -75,5 +86,5 @@ echo ""
 echo "  Success, system is restarting..."
 echo ""
 
-[[ "$fqlx" == "AB" ]] && $fastboot_cmd set_active a
-$fastboot_cmd reboot
+[[ "$slot" == "AB" ]] && fastboot set_active a
+fastboot reboot
